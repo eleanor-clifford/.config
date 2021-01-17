563907eea15f7a023be82220c6d0e166f1f12a88
Tim Clifford <tclifford@protonmail.com>
2021-01-17 15:55:15 +0000

    Bugfix

--- i3/setup-disp_backg_bar.sh
+++ i3/setup-disp_backg_bar.sh
@@ -4,7 +4,3 @@ else
 	$HOME/.config/scripts/rotatenormal-norestart.sh
 fi
 feh --bg-fill $HOME/.config/i3/lock-cache/resized.png --geometry +0
-MONITOR=eDP $HOME/.config/polybar/launch $HOME/.config/polybar/config top
-MONITOR=eDP $HOME/.config/polybar/launch $HOME/.config/polybar/config bottom
-MONITOR=DisplayPort-0 $HOME/.config/polybar/launch $HOME/.config/polybar/config top
-MONITOR=DisplayPort-0 $HOME/.config/polybar/launch $HOME/.config/polybar/config external_bottom
