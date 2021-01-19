6e66150b4b5f91bde2fa2d3418802a4f58c7966b
Tim Clifford <tclifford@protonmail.com>
2021-01-19 22:40:45 +0000

    Fix no exit prompt on `aur sync`

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if [ (tty) = "/dev/tty1" ]
 end
 
 if tty | grep -q "/dev/tty"
-	$HOME/.config/scripts/set_tty_colors.sh
+	$HOME/scripts/set_tty_colors.sh
 end
 
 #if ! pgrep -u "$USER" ssh-agent > /dev/null; then
@@ -15,9 +15,7 @@ end
 #fi
 
 export BROWSER=/usr/bin/firefox
-export EDITOR=/usr/bin/vim
-export VISUAL=/usr/bin/vim
-export AUR_PAGER=ranger
+export AUR_PAGER="$HOME/.config/scripts/rangerp.sh"
 
 alias extend-hdmi="xrandr --output eDP-1-1 --auto --output HDMI-1-1 --auto --right-of eDP-1-1"
 alias clone-hdmi="xrandr --output eDP-1-1 --auto --output HDMI-1-1 --auto --same-as eDP-1-1"
@@ -26,7 +24,7 @@ alias only-hdmi="xrandr --output eDP-1-1 --off --output HDMI-1-1 --auto"
 alias update-wallpapers="sudo systemctl restart wallpapers"
 alias vectorise-notebooks="./OneDrive/ExamplePapers/process.sh"
 alias start-vm='qemu-system-x86_64 -drive file=win_img,format=raw,cache=none,if=virtio -m 4G -net nic -net user,smb=/ -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time -smp 3 -enable-kvm'
-alias start-rfx='$HOME/.config/scripts/rfx.sh'
+alias start-rfx='/home/tim/scripts/rfx.sh'
 #alias start-remote='ssh raspberryfry "wakeonlan -p 9 -i 192.168.1.255 d0:17:c2:d3:26:9d"'
 #alias shutdown-remote='
 #	echo -n "[sudo] password for tim: "
@@ -36,10 +34,10 @@ alias start-rfx='$HOME/.config/scripts/rfx.sh'
 #'
 alias remote-power='ssh raspberryfry "python3 remote-powerbutton/pressquick.py"'
 alias remote-powerlong='ssh raspberryfry "python3 remote-powerbutton/presslong.py"'
-alias start-remote='$HOME/.config/scripts/start-remote.sh'
-alias mount-all='$HOME/.config/scripts/mount-all.sh'
-alias unmount-all='$HOME/.config/scripts/unmount-all.sh'
-alias umount-all='$HOME/.config/scripts/unmount-all.sh'
+alias start-remote='/home/tim/scripts/start-remote.sh'
+alias mount-all='/home/tim/scripts/mount-all.sh'
+alias unmount-all='/home/tim/scripts/unmount-all.sh'
+alias umount-all='/home/tim/scripts/unmount-all.sh'
 alias aur-remove='repo-remove /var/cache/pacman/custom/custom.db.tar'
 alias nano=vim
 alias duplicate-screen='xrandr --output eDP --auto --output $OUTPUT --mode 1920x1080 --same-as eDP --panning 1920x1080+0+0'
---
+++ scripts/rangerp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ranger $1
+read -p "Press enter to continue or ctrl+d to exit " tmp
