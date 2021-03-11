aacedd2d0e486e08ab709238f2d27e6ae03ea452
Tim Clifford <tclifford@protonmail.com>
2021-03-11 16:51:01 +0000

    Fix polybar player

--- scripts/islandscape.sh
+++ scripts/islandscape.sh
@@ -1,3 +1,4 @@
 #!/bin/dash
 # This script used to work on any screen size but i deleted it by accident, now we have this
-xrandr | egrep -q "connected.*1920x1080"
+xrandr | sed -n 's/.* connected[^[:digit:]]*\([^x]\+\)x\([^+]\+\).*/\1 > \2/p'\
+       | bc -l | egrep -q "^1$"
